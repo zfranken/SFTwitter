27ca01f1bacb0ba03369cbf93fe19551a8bd7ee3
DFC\ZAF <zaf@deltafaucet.com>
2017-10-27 08:44:19 -0400

    a bit of cleanup

--- src/main/java/sftwitter/SFTwitterService.java
+++ src/main/java/sftwitter/SFTwitterService.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import twitter4j.MediaEntity;
@@ -13,18 +14,39 @@ import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 @Component
 public class SFTwitterService {
 
 	private static final Logger logger = Logger.getLogger(SFTwitterService.class.getName());
-	
+
+	@Value("${twitter.username}")
+	private String twitterUsername;
+
+	@Value("${twitter.api.consumerkey}")
+	private String twitterApiConsumerKey;
+
+	@Value("${twitter.api.consumersecret}")
+	private String twitterApiConsumerSecret;
+
+	@Value("${twitter.api.accesstoken}")
+	private String twitterApiAccessToken;
+
+	@Value("${twitter.api.accesstokensecrect}")
+	private String twitterApiAccessTokenSecret;
+
 	@Autowired
 	Twitter twitter;
 	
-	@Value("${twitter.username}")
-    private String twitterUsername;
 
+	/**
+	 * Get the top n tweets from the twitter feed
+	 * 
+	 * @param numberOfTweets
+	 * @return List of SFTweets
+	 */
 	public List<SFTweet> getLatestTweets(Integer numberOfTweets) {
 
 		String user = twitterUsername;
@@ -33,18 +55,24 @@ public class SFTwitterService {
 		try {
 			Paging page = new Paging(1, numberOfTweets);
 			List<Status> statuses = twitter.getUserTimeline(user, page);
-			for(Status status : statuses) {
+			for (Status status : statuses) {
 				tweets.add(buildSFTweet(status));
 			}
 		} catch (TwitterException e) {
 			e.printStackTrace();
 		}
-		
+
 		return tweets;
 	}
-	
+
+	/**
+	 * Convert the twitter4j status into a SFTweet
+	 * 
+	 * @param status The twitter4j tweet object
+	 * @return SFTweet populated from the twitter4j status object
+	 */
 	public SFTweet buildSFTweet(Status status) {
-				
+
 		SFTweet sfTweet = new SFTweet();
 		sfTweet.setScreenName(status.getUser().getScreenName());
 		sfTweet.setUserName(status.getUser().getName());
@@ -53,13 +81,24 @@ public class SFTwitterService {
 		sfTweet.setRetweetCount(status.getRetweetCount());
 		sfTweet.setTweetDate(status.getCreatedAt());
 
-		//get tweet images
+		// get tweet images
 		MediaEntity[] media = status.getMediaEntities();
-		for(MediaEntity m : media){ 
-		    sfTweet.addTweetImageUrl(m.getMediaURLHttps());
+		for (MediaEntity m : media) {
+			sfTweet.addTweetImageUrl(m.getMediaURLHttps());
 		}
-		
+
 		return sfTweet;
 	}
 
+	
+	@Bean
+	public Twitter twitter() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setOAuthConsumerKey(twitterApiConsumerKey);
+		cb.setOAuthConsumerSecret(twitterApiConsumerSecret);
+		cb.setOAuthAccessToken(twitterApiAccessToken);
+		cb.setOAuthAccessTokenSecret(twitterApiAccessTokenSecret);
+
+		return new TwitterFactory(cb.build()).getInstance();
+	}
 }
